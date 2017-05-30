4837d887b7a47bb7dd4406f3faf926b47797374a
James Shaw <js102@zepler.net>
2017-05-30 20:43:17 +0100

    add sumFields to deb

--- debian/override_dh_auto_build.sh
+++ debian/override_dh_auto_build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-wmake all applications/utilities/postProcessing/globalSum
+set -e
+#TODO: make sure src libraries can see headers from other src libraries
+#wmake -j -a src
+#wmake -j all applications/utilities/postProcessing
+wmake -j applications/utilities/postProcessing/globalSum
+wmake -j applications/utilities/postProcessing/sumFields
