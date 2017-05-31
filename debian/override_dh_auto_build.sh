a0b3df7e3f97f3801bf6f2b402896de2331252f6
James Shaw <js102@zepler.net>
2017-05-31 08:42:22 +0100

    build all apps and libs

--- debian/override_dh_auto_build.sh
+++ debian/override_dh_auto_build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
-#TODO: make sure src libraries can see headers from other src libraries
-#wmake -j -a src
-#wmake -j all applications/utilities/postProcessing
-wmake -j applications/utilities/postProcessing/globalSum
-wmake -j applications/utilities/postProcessing/sumFields
+wmake -j -a src
+wmake -j -a applications
