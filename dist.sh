a2acc1ec473c2b608f77f5e277d15adfecc6e2de
James Shaw <js102@zepler.net>
2017-06-25 11:34:09 +0100

    UBUNTU_CODENAME not present in trusty

--- dist.sh
+++ dist.sh
@@ -2,10 +2,10 @@
 set -e
 
 VERSION=$(date +"%Y%m%d%H%M%S")
-source /etc/os-release
+source /etc/lsb-release
 
 # create a skeletal debian/changelog
-dch --create --package "atmosfoam-tools" --distribution $UBUNTU_CODENAME --newversion=$VERSION "Build for $(git rev-parse HEAD)"
+dch --create --package "atmosfoam-tools" --distribution $DISTRIB_CODENAME --newversion=$VERSION "Build for $(git rev-parse HEAD)"
 
 # create an amd64 binary package
 # using the Makefile 'debian/rules'
@@ -14,4 +14,4 @@ debuild -i -us -uc -b
 # upload to the debian apt repository located in the
 # Amazon S3 bucket.  deb-s3 expects AWS_ACCESS_KEY_ID and
 # AWS_SECRET_ACCESS_KEY environment variables to be set.
-deb-s3 upload --bucket atmosfoam-apt --codename=$UBUNTU_CODENAME --component=dev ../atmosfoam-tools_${VERSION}_amd64.deb
+deb-s3 upload --bucket atmosfoam-apt --codename=$DISTRIB_CODENAME --component=dev ../atmosfoam-tools_${VERSION}_amd64.deb
