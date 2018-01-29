9965afe1dd77e9ea4dcbbd5e3d58c613525a3619
James Shaw <js102@zepler.net>
2018-01-29 14:20:18 +0000

    pass PACKAGE_VERSION in, not out

--- dist.sh
+++ dist.sh
@@ -2,22 +2,22 @@
 set -e
 
 display_usage() {
-	echo -e "Usage: dist.sh <codename>\n"
+	echo -e "Usage: dist.sh <package_version> <codename>\n"
 }
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
 	display_usage
 	exit 1
 fi
 
-VERSION=$(date +"%Y%m%d%H%M%S")
-CODENAME=$1
+PACKAGE_VERSION=$1
+CODENAME=$2
 
 # create a skeletal debian/changelog
 SINGULARITYENV_DEBFULLNAME=$DEBFULLNAME \
 SINGULARITYENV_DEBEMAIL=$DEBEMAIL \
-singularity exec -e $CODENAME.img dch --create --package "atmosfoam-tools" --distribution $CODENAME --newversion=$VERSION "Build for $(git rev-parse HEAD)"
+singularity exec -e $CODENAME.img dch --create --package "atmosfoam-tools" --distribution $CODENAME --newversion=$PACKAGE_VERSION "Build for $(git rev-parse HEAD)"
 
 # create an amd64 binary package
 # using the Makefile 'debian/rules'
@@ -30,8 +30,7 @@ singularity exec -e $CODENAME.img debuild -i -us -uc -b
 # upload to the debian apt repository located in the
 # Amazon S3 bucket.  deb-s3 expects AWS_ACCESS_KEY_ID and
 # AWS_SECRET_ACCESS_KEY environment variables to be set.
-deb-s3 upload --bucket atmosfoam-apt --codename=$CODENAME --component=dev ../atmosfoam-tools_${VERSION}_amd64.deb
+deb-s3 upload --bucket atmosfoam-apt --codename=$CODENAME --component=dev ../atmosfoam-tools_${PACKAGE_VERSION}_amd64.deb
 
 # ensure that the new .deb can be installed
 sudo singularity exec -e -w $CODENAME.img add-apt-repository "http://atmosfoam-apt.s3-website-eu-west-1.amazonaws.com dev" -y
-echo $VERSION
--- singularity.dist.artful.sh
+++ singularity.dist.artful.sh
@@ -3,9 +3,10 @@ set -e
 
 VERSION=17.10
 CODENAME=artful
+PACKAGE_VERSION=$(date +"%Y%m%d%H%M%S")
 
-PACKAGE_VERSION=$(./singularity.bootstrap.sh $VERSION $CODENAME)
-./dist.sh $CODENAME
+./singularity.bootstrap.sh $VERSION $CODENAME
+./dist.sh $PACKAGE_VERSION $CODENAME
 sudo singularity exec -e -w $CODENAME.img apt-get update -qq --allow-insecure-repositories
 sudo singularity exec -e -w $CODENAME.img apt-get install atmosfoam-tools=$PACKAGE_VERSION -y --allow-unauthenticated --no-install-recommends
 git clean -xfd .
