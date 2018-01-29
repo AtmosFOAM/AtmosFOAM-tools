be5759e977d8685ba3380489028932db8b432a8b
James Shaw <js102@zepler.net>
2018-01-29 12:43:20 +0000

    update to ubuntu 17.10

--- dist.sh
+++ dist.sh
@@ -33,7 +33,8 @@ singularity exec -e $CODENAME.img debuild -i -us -uc -b
 deb-s3 upload --bucket atmosfoam-apt --codename=$CODENAME --component=dev ../atmosfoam-tools_${VERSION}_amd64.deb
 
 # ensure that the new .deb can be installed
-sudo singularity exec -e -w $CODENAME.img apt-get update -qq
+sudo singularity exec -e -w $CODENAME.img add-apt-repository "http://atmosfoam-apt.s3-website-eu-west-1.amazonaws.com dev" -y
+sudo singularity exec -e -w $CODENAME.img apt-get update -qq --allow-insecure-repositories
 sudo singularity exec -e -w $CODENAME.img apt-get install atmosfoam-tools=$VERSION -y --allow-unauthenticated --no-install-recommends
 
 git clean -xfd .
