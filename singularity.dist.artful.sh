1205cca612ff2660263e8d504d45949561ee5451
James Shaw <js102@zepler.net>
2018-01-29 14:03:13 +0000

    remove CLI args check from singularity.dist scripts

--- singularity.dist.artful.sh
+++ singularity.dist.artful.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -e
 
-display_usage() {
-	echo -e "Usage: singularity.bootstrap.sh <version> <codename>\n"
-}
-
-if [ $# -le 1 ]
-then
-	display_usage
-	exit 1
-fi
-
-export VERSION=17.10
-export CODENAME=artful
+VERSION=17.10
+CODENAME=artful
 
 ./singularity.bootstrap.sh $VERSION $CODENAME
 ./dist.sh $CODENAME
--- singularity.dist.trusty.sh
+++ singularity.dist.trusty.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -e
 
-display_usage() {
-	echo -e "Usage: singularity.bootstrap.sh <version> <codename>\n"
-}
-
-if [ $# -le 1 ]
-then
-	display_usage
-	exit 1
-fi
-
-export VERSION=14.04
-export CODENAME=trusty
+VERSION=14.04
+CODENAME=trusty
 
 ./singularity.bootstrap.sh $VERSION $CODENAME
 ./dist.sh $CODENAME
